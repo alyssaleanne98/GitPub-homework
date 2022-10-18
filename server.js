0d2bb364220b93682248b720bbd8e813f0458f16
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-17 20:46:36 -0700

    Linking index.ejs to show.ejs

--- server.js
+++ server.js
@@ -13,48 +13,8 @@ app.get('/', function (req, res) {
 
 // set up your database 
 
-const drinks = [
-    {
-      name: 'cruddy mary',
-      price: 132,
-      image: 'https://i.imgur.com/Va5iIw5.jpg',
-    },
-    {
-      name: 'index on the beach',
-      price: 68,
-      image: 'https://i.imgur.com/XV2aPa2.jpg',
-    },
-    {
-      name: 'hack & coke',
-      price: 1,
-      image: 'https://i.imgur.com/rLOXFRI.jpg',
-    },
-    {
-      name: 'whiskey-value pair',
-      price: 11,
-      image: 'https://i.imgur.com/a7rmkoS.jpg',
-    },
-    {
-      name: '404 horsemen',
-      price: 202,
-      image: 'https://i.imgur.com/FLucOLr.jpg',
-    },
-    {
-      name: 'if stateMint julep',
-      price: 2,
-      image: 'https://i.imgur.com/yaoK0Dg.jpg',
-    },
-    {
-      name: 'APIPA',
-      price: 43,
-      image: 'https://i.imgur.com/qAhA7pi.jpg',
-    },
-    {
-      name: 'node to joy',
-      price: 56,
-      image: 'https://i.imgur.com/MbVdwZz.jpg',
-    },
-  ];
+const drinks = require('./models/drinks')
+    
 
 // get route 
  app.get('/getroute', (req, res) => {
@@ -63,19 +23,22 @@ const drinks = [
 
  // drinks route 
  app.get('/drinks', (req, res) => {
-    res.render('index.ejs', {
+    res.render('drinksindex.ejs', {
         drinks: drinks,
     });
  })
 
  // show route 
  app.get('/drinks/:id', (req, res) => {
-    res.send(req.params.id);
+    // res.send(req.params.id)
+    res.render('drinksshow.ejs', {
+        drinks: drinks[req.params.id],
+    });
  })
 
 
  // tell express to listen for requests from the browser (client) 
  // make sure that this is located at bottom of code 
 app.listen(3000, () => {
-    console.log('listening');
+    console.log('your server is running');
 })
